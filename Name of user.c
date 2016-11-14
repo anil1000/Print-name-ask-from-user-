b5fcbcbd4cc89b207a488ea994651f23e683af56
anil1000 <anil.paudel101@gmail.com>
2016-11-14 12:57:19 +0545

    Add files via upload

---
+++ Name of user.c
@@ -0,0 +1,24 @@
+ /*
+Student Name: Anil Paudel
+Subject: Programming Fundamental
+Roll No.: 02
+Lab Sheet No.: 3
+Program: Write a C program print My name
+Date: November 14,2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+   int main (){
+   char name [20];
+ printf("What is Your Name??\n");
+   scanf("%s",&name);
+ printf("Name of user is:%s",name);
+
+ getch();
+
+ return (0);
+
+
+   }
